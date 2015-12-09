670ae75313553a10166df5b1d2093ca28b87e848
Cory Erickson <ericksco@botto.erickson.org>
2015-12-09 17:01:57 -0600

    added mysql test script

---
+++ testdb.php
@@ -0,0 +1,28 @@
+<?php
+
+$server = 10.1.0.4;
+$username = "appl";
+$password = "appl";
+
+$db = mysqli_connect($server, $username, $password);
+
+if (!$db)
+{
+	die("Couldn't connect to MySQL\n");
+}
+
+mysqli_select_db("appl", $db);
+
+$res = mysqli_query("select * from x", $db);
+
+if (mysqli_num_rows($res) > 0)
+{
+	while ($row = mysqli_fetch_assoc($res))
+	{
+		print "value = $row["x"]\n";
+	}
+}
+
+mysqli_close($db);
+
+?>
